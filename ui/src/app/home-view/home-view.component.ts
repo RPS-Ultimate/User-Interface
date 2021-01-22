fdc18831a6b266cb25f50f68c0259f1cf0355a1d
Daniel Hopson <hdhopson88@yahoo.com>
2021-01-22 03:01:09 -0800

    Creates home, and match view

---
+++ ui/src/app/home-view/home-view.component.ts
@@ -0,0 +1,20 @@
+import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home-view',
+  templateUrl: './home-view.component.html',
+  styleUrls: ['./home-view.component.css']
+})
+export class HomeViewComponent implements OnInit {
+
+  constructor(private router: Router) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe();
+  }
+
+  startNewMatch() {
+    this.router.navigate(['match']);
+  }
+}
