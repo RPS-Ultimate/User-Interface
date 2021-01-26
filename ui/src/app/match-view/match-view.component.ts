aeff59074ad382f2bd8f5b13b9030200c2522f04
Hopson JR <2005998@cognizant.com>
2021-01-25 21:30:30 -0600

    Adds png assets, updates html

--- ui/src/app/match-view/match-view.component.ts
+++ ui/src/app/match-view/match-view.component.ts
@@ -1,5 +1,3 @@
-import { Observable } from 'rxjs';
-import { ImageService } from './../services/image/image.service';
 import { Outcome } from './../models/outcome.model';
 import { OutcomeService } from './../services/outcome/outcome.service';
 import { Component, OnInit } from '@angular/core';
@@ -11,25 +9,20 @@ import { Router } from '@angular/router';
   styleUrls: ['./match-view.component.css']
 })
 export class MatchViewComponent implements OnInit {
-  outcome: Outcome;
-  message: String;
-  gameIsInSession: Boolean;
-  userImg: String = "../../assets/rock-icon.png";
-  botImg: String = "../../assets/rock-icon.png";
-  moves: string[];
+  gameIsInSession: Boolean = true;
+  message: String = "Rock, paper, scissors, shoot!";
+  userImg: String = "../../assets/Ready(0)-icon(0).png";
+  botImg: String = "../../assets/Ready-icon(0).png";
+  moves: String[] = ["Rock", "Paper", "Scissors"];
+  outcome: Outcome = {
+    userMove: 0,
+    botMove: 0,
+    userWins: false,
+    isADraw: false
+  };
   
-  constructor(private outcomeService: OutcomeService, private imageService: ImageService, private router: Router) { 
-    this.outcome = {
-      userMove: 0,
-      botMove: 0,
-      userWins: false,
-      isADraw: false
-    };
-    this.message = "Rock, paper, scissors, shoot!";
-    // this.userImg = new Observable();
-    // this.botImg = new Observable();
-    this.gameIsInSession = true;
-    this.moves = ["", "Rock", "Paper", "Scissors"];
+  constructor(private outcomeService: OutcomeService, private router: Router) { 
+    
   }
 
   ngOnInit(): void {
@@ -39,21 +32,17 @@ export class MatchViewComponent implements OnInit {
   getOutcome(userMove: number) {
     this.outcomeService.getOutcome(userMove).subscribe((res) => {
       this.outcome = res;
-      this.userImg = `../../assets/${this.moves[this.outcome.userMove]}-icon.png`
-      this.botImg = `../../assets/${this.moves[this.outcome.botMove]}-icon.png`
+      this.userImg = `../../assets/${this.moves[this.outcome.userMove]}-icon(0).png`
+      this.botImg = `../../assets/${this.moves[this.outcome.botMove]}-icon(0).png`
       if (this.outcome.isADraw) {
         this.message = "It's a draw. Continue the match...";
       } else if (this.outcome.userWins) {
-        this.message = `Your ${this.moves[this.outcome.userMove]} beats ${this.moves[this.outcome.botMove]}! You Win!`
+        this.message = `Your ${this.moves[this.outcome.userMove]} beats ${this.moves[this.outcome.botMove]}! You Win!`;
         this.gameIsInSession = false;
       } else {
-        this.message = `${this.moves[this.outcome.botMove]} beats your ${this.moves[this.outcome.userMove]}... You Lose...`
+        this.message = `${this.moves[this.outcome.botMove]} beats your ${this.moves[this.outcome.userMove]}... You Lose...`;
         this.gameIsInSession = false;
       }
-
-      // this.userImg = this.getImage(this.outcome.userMove);
-
-      console.log(this.outcome);
     });
   }
 
@@ -65,8 +54,4 @@ export class MatchViewComponent implements OnInit {
   homeMenu() {
     this.router.navigate([""]);
   }
-
-  // getImage(move: number): Observable<any> {
-  //   return this.imageService.getImage(move);
-  // }
 }
--- ui/src/app/models/API_KEY.model.ts
+++
@@ -1,3 +0,0 @@
-export class API_KEY {
-    client_id: String = "umagOS2j4UkiIqBE1dEO1kKrBNtRZ9HTYKSPwGHOjNA";
-}
